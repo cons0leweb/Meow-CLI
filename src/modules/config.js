74c4f4409050232d076f8400b1480c7f97c6607c
cons0leweb <cons0leweb@users.noreply.github.com>
2026-05-28 21:21:59 +0300

    patch src/modules/config.js

--- src/modules/config.js
+++ src/modules/config.js
@@ -114,6 +114,7 @@ const DEFAULT_CONFIG = {
   providers: {},
   mcp_servers: {},
   active_provider: "",
+  api_schema: "openai",
   meowcube_url: process.env.MEOWCUBE_URL || "https://meowcube.space",
   trust_url: "https://raw.githubusercontent.com/meowcli/meow-cli/main/globals/trust/trust.meow",
   templates: {
