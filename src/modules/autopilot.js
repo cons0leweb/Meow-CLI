12037135a29a9378443d190845df02aeca9cdd6b
cons0leweb <cons0leweb@users.noreply.github.com>
2026-05-28 22:33:53 +0300

    patch src/modules/autopilot.js

--- src/modules/autopilot.js
+++ src/modules/autopilot.js
@@ -12,6 +12,7 @@ import { executeTool, runShell } from "./tools.js";
 import { getTrustManager, TRUST_LEVEL } from "./trust.js";
 import { PromptOptimizer } from "./smart/prompt-optimizer.js";
 import { sanitizeToolCallsForApi } from "./images.js";
+import { CoordinationChannel, formatCoordinationMessage } from "./autopilot-channel.js";
 
 /** @type {Object} Autopilot execution phases */
 const PHASE = {
